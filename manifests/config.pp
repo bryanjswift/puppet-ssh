8648bd0df6c0a3074bf5c85d40dee70bd17416f0
Bryan J Swift <bryan.j.swift@gmail.com>
2012-12-14 02:14:47 -0500

    Fix onlyif match statements to make augeas things more selective

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,23 +5,9 @@ class ssh::config {
   $ssh_ports = $ssh::ports
   $ssh_users = $ssh::users
 
-  /* This needs to be thought through a bit more
-   * I'm not sure how reliant on the defaults I want to be * /
   augeas { "sshd_config":
     context => "/files${ssh::params::service_config}",
     changes => template('ssh/sshd_config_augeas.erb'),
-    require => Class["ssh::install"],
-    notify  => Class["ssh::service"],
-  }
-  /* End thinking time code */
-
-  file { "sshd_config":
-    ensure  => present,
-    owner   => "root",
-    group   => "root",
-    mode    => 0600,
-    path    => $ssh::params::service_config,
-    content => template("ssh/sshd_config.erb"),
     require => Class["ssh::install"],
     notify  => Class["ssh::service"],
   }
--- manifests/user.pp
+++ manifests/user.pp
@@ -55,7 +55,7 @@ define ssh::user(
     augeas { "sshd_match_user_${name}":
       context => "/files${ssh::params::service_config}",
       changes => template('ssh/sshd_config_match_user.erb'),
-      onlyif  => "match Match[Condition/User = ${name}] size > 0",
+      onlyif  => "match Match[Condition/User = \"${name}\"] size > 0",
       require => [Class['ssh::config']],
       notify  => [Class['ssh::service']],
     }
@@ -63,7 +63,7 @@ define ssh::user(
     augeas { "sshd_match_user_${name}_new":
       context => "/files${ssh::params::service_config}",
       changes => template('ssh/sshd_config_match_user_new.erb'),
-      onlyif  => "match Match[Condition/User = ${name}] size == 0",
+      onlyif  => "match Match[Condition/User = \"${name}\"] size == 0",
       require => [Class['ssh::config']],
       notify  => [Class['ssh::service']],
     }
